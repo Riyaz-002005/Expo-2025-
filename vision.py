e773fd8b79750212ff336f35831e7f46c776a95d
Mohamed Umar Riyaz A <155342036+Riyaz-002005@users.noreply.github.com>
2025-02-10 20:52:00 +0530

    Add files via upload

---
+++ vision.py
@@ -0,0 +1,80 @@
+import streamlit as st  
+st.set_page_config(page_title="Image Recognition App" , page_icon=":camera:", layout="centered", initial_sidebar_state="expanded")
+
+from dotenv import load_dotenv
+load_dotenv()
+
+import os
+import google.generativeai as genai
+from PIL import Image
+import base64
+
+os.getenv("GOOGLE_API_KEY")
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+model=genai.GenerativeModel("gemini-1.5-flash")
+def get_gemini_response(input, image):
+    if input != "":
+        response = model.generate_content([input, image])
+    else:
+        response = model.generate_content(image)
+    return response.text
+
+def set_background(image_file):
+    with open(image_file, "rb") as f:
+        data = f.read()
+    encoded = base64.b64encode(data).decode()
+    st.markdown(
+        f"""
+        <style>
+        .stApp {{
+            background-image: url(data:image/png;base64,{encoded});
+            background-size: cover;
+        }}
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
+# Set the background image
+set_background("images 3.jpg")
+
+st.markdown("<h1 style='font-style: italic;'>ChatBot with Image Recognition</h1>", unsafe_allow_html=True)
+input = st.text_input(" ChatBox: ", key="input")
+
+uploaded_file = st.file_uploader("Upload or Drag and Drop an image... ", type=["jpg", "jpeg", "png"])
+image = ""
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption='Uploaded Image.', use_container_width=True)
+
+st.markdown(
+    """
+    <style>
+    .stButton>button {
+        transition: all 0.3s ease;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        background-color: #28a745;
+        color: white;
+        border: 2px solid black;
+    }
+    .stButton>button:hover {
+        transform: scale(1.05);
+        box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
+        background-color: #218838;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+submit=st.button("Generate Response")
+
+if submit:
+    if image is None:
+        st.warning("Please upload an image first.", icon="⚠️")
+    else:
+        with st.spinner("Generating response..."):
+            response = get_gemini_response(input, image)
+        st.success("Response generated!", icon="✅")
+        st.write(response)
